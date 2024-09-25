df63be5c23a11c695b75d818cc0c1390823bdc8f
Lucas Kang <kangzundao@gmail.com>
2024-09-25 16:03:52 -0400

    updated post on server.js

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,28 @@ import express from 'express';
 import {connectToDatabase} from "./dbconnection.js";
 import { fetchBooks, addBook } from './api.js';
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+const app = express(); //creates instance of Express app
+const PORT = process.env.PORT; //sets port number from .env
 
-app.use(express.json());
+app.use(express.json()); //middleware setup - converts JSON to JS object
 
-connectToDatabase();
+connectToDatabase(); //connects to Mongo
 
-app.get('api/books', async(req, res) => {
-    const books = await fetchBooks();
-    res.json(books);
+app.get('/api/books', async(req, res) => {
+    const books = await fetchBooks(); //retrieve books from database
+    res.json(books); //sends books to client (frontend)
 });
 
-app.post('api/books', async(req, res) => {
-    const books = await fetchBooks();
-    res.json(books);
+app.post('/api/books', async(req, res) => {
+    const newBook = req.body;
+
+    try {
+        const result = await addBook(newBook);
+        res.status(201).json(result);
+    } catch(error) {
+        console.error("Error adding book:", error);
+        res.status(500).json({message: "Failed to add book"});
+    }
 });
 
 app.listen(PORT, () => {
