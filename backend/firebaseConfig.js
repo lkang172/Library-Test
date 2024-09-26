c7f2d2b232d4eae0a127321407e921c640519439
Lucas Kang <kangzundao@gmail.com>
2024-09-26 17:34:38 -0400

    dotenv

--- backend/firebaseConfig.js
+++ backend/firebaseConfig.js
@@ -1,13 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import dotenv from 'dotenv';
+dotenv.config();
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD-PvDs-4b51YlmSDz8GwR30HUnwavJSZI",
+  apiKey: `${process.env.FIREBASE_API_KEY}`,
   authDomain: "halcyonx-f7af3.firebaseapp.com",
   projectId: "halcyonx-f7af3",
   storageBucket: "halcyonx-f7af3.appspot.com",
